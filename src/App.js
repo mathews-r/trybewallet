6028ca7e635be18052f26969ccf54068805dfa17
Mathews <mathewsrodrigues9@gmail.com>
2022-10-28 17:03:09 -0300

    ajustando rotas

--- src/App.js
+++ src/App.js
@@ -1,14 +1,16 @@
 import React from "react";
-import { Route } from "react-router-dom";
+import { Routes, Route } from "react-router-dom";
+
 import Login from "./pages/Login";
 import Wallet from "./pages/Wallet";
 
 function App() {
   return (
     <main>
-      <p>Falhando</p>
-      {/* <Route path="/" component={Login} />
-      <Route path="/carteira" component={Wallet} /> */}
+      <Routes>
+      <Route path="/" element={<Login />} />
+      <Route path="/carteira" element={<Wallet />} />
+      </Routes>
     </main>
   );
 }
--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import { BrowserRouter } from "react-router-dom";
+import { HashRouter } from "react-router-dom";
 import { Provider } from "react-redux";
 
 import store from "./redux/store";
@@ -8,11 +8,11 @@ import App from "./App";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  <BrowserRouter>
+  <HashRouter>
     <Provider store={store}>
       <App />
     </Provider>
-  </BrowserRouter>
+  </HashRouter>
 );
 
 // If you want to start measuring performance in your app, pass a function
